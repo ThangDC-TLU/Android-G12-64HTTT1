3c47cb773485c7097886ed2168bf1d7374195a5e
ThangDC-TLU <dinhcaothang08102004@gmail.com>
2025-05-22 17:30:30 +0700

    design model

---
+++ app/src/main/java/com/example/ezilish/models/Topic.java
@@ -0,0 +1,19 @@
+package com.example.ezilish.models;
+
+public class Topic {
+    private String name;
+
+    public Topic() {}
+
+    public Topic(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
