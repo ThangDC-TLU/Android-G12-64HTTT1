83b4562f5b2d2659ac6f718d32f1d36cbb1ab44d
Hoàng Anh Vũ <hoanganhvuz2103@gmail.com>
2025-06-21 23:10:28 +0700

    quiz API

---
+++ app/src/main/java/vn/edu/tlu/dinhcaothang/ezilish/activities/QuizQuestion.java
@@ -0,0 +1,9 @@
+package vn.edu.tlu.dinhcaothang.ezilish.activities;
+
+import java.util.List;
+
+public class QuizQuestion {
+    public String question;
+    public List<String> options; // A, B, C, D
+    public String answer;        // "A", "B", "C", hoặc "D"
+}
