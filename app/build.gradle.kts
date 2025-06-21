f3c273c0d6714b66b5103a621d2dbc4aad2a655d
Hoàng Anh Vũ <hoanganhvuz2103@gmail.com>
2025-06-21 23:11:13 +0700

    build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.firebase.database)
